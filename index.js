b4a2d960427f06cb3c0238b51799b5744f5bdc72
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-07-13 12:55:23 +0530

    added user routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './src/routes/user.js';
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
+
+app.use('/user', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
