60a2800cca8cf2ba59fa3a25b99fbc81da900ced
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-07-13 13:31:09 +0530

    made edit question api

--- src/controllers/question.js
+++ src/controllers/question.js
@@ -27,3 +27,52 @@ export const createQuestion = async (req, res) => {
       res.status(500).json({ message: error.message });
     }
   };
+
+  export const updateQuestion = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const question = req.body;
+  
+      // Check if user is authenticated
+      if (!req.user) {
+        return res.status(401).json({ message: 'Unauthenticated.' });
+      }
+  
+      // Validate the post ID
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ message: 'Invalid question ID' });
+      }
+  
+      // Find the post by ID
+      const existingQuestion = await Question.findById(id);
+  
+      // Check if the post exists
+      if (!existingQuestion) {
+        return res.status(404).json({ message: 'Question not found.' });
+      }
+  
+      // Check if the user is authorized to update the post
+      if (existingQuestion.creator._id.toString() !== req.user) {
+        return res.status(403).json({ message: 'Not authorized to update this question.' });
+      }
+  
+      // Check if the content of req and existing question is the same
+      if (
+        question.question === existingQuestion.question
+      ) {
+        return res.status(400).json({ message: 'The provided content is the same as the existing  question.' });
+      }
+  
+      // Update the post
+      const updatedQuestion = await Question.findByIdAndUpdate(
+        id,
+        { ...question, id, updatedAt: new Date() },
+        { new: true }
+      );
+  
+      res.status(200).json(updatedQuestion);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: error.message });
+    }
+  };
--- src/routes/question.js
+++ src/routes/question.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createQuestion } from '../controllers/question.js';
+import { createQuestion, updateQuestion } from '../controllers/question.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 import { checkAdmin } from '../middleware/roleCheck.js';
 
 const router = express.Router();
 
 router.post('/', [verifyToken, checkAdmin], createQuestion);
+router.patch('/:id', [verifyToken, checkAdmin], updateQuestion);
 
 export default router;
